e9c089e0685e5edd1043484ab1287aa4c5ae8522
bitnerrd <irfanabid.913@gmail.com>
2024-05-24 00:22:17 +0500

    fizes

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -2,15 +2,22 @@ import User from "../models/userModel.js";
 import Activity from "../models/activityModel.js";
 
 export const Addactivity = async (req, res) => {
-  const { activityName, phone, activityTime, bookingDate } = req.body;
-  const user = req.user;
-
-  const checkuser = await User.findById(user._id);
-  if (!checkuser) {
-    return res.status(404).send({ Message: "User Not Found" });
-  }
-
   try {
+    const { activityName, phone, activityTime, bookingDate } = req.body;
+    for (let i in req.body) {
+      if (!req.body[i]) {
+        return res
+          .status(400)
+          .send({ message: "All fields must be provided", success: false });
+      }
+    }
+    const user = req.user;
+
+    const checkuser = await User.findById(user._id);
+    if (!checkuser) {
+      return res.status(404).send({ message: "User Not Found" });
+    }
+
     const datasave = new Activity({
       activityName: activityName,
       phone: phone,
@@ -19,9 +26,13 @@ export const Addactivity = async (req, res) => {
       userId: user._id,
     }).save();
 
-    return res.status(200).send({ Message: "Activity Created Successfully" });
+    return res
+      .status(200)
+      .send({ message: "Activity Created Successfully", success: true });
   } catch (error) {
-    return res.status(400).send({ Message: "Activity Creation Failed", error });
+    return res
+      .status(400)
+      .send({ message: "Activity Creation Failed", error, success: false });
   }
 };
 
@@ -29,37 +40,43 @@ export const GetactivityBYID = async (req, res) => {
   try {
     const { id } = req.params;
     const activity = await Activity.findById(id);
-    return res.status(200).send(activity);
+    return res
+      .status(200)
+      .json({ message: "Activity Found", data: activity, success: true });
   } catch (error) {
-    console.log(error.message);
+    return res
+      .status(404)
+      .send({ message: "Activity Not Found", success: false });
   }
 };
 
 export const activites = async (req, res) => {
   const user = req.user;
   try {
-    const userfind = await Activity.find({ userId: user._id }).populate(
-      "userId"
-    );
-    return res.status(200).send(userfind);
+    const users = await Activity.find({ userId: user._id }).populate("userId");
+    return res
+      .status(200)
+      .json({ message: "activites found", data: users, success: true });
   } catch (error) {
-    console.log(error.message);
+    return res.status(400).json({
+      message: "error while getting all ativites",
+      success: false,
+    });
   }
 };
 
 export const Updateactivity = async (req, res) => {
-  const { id } = req.params;
-  const { activityName, phone, activityTime, bookingDate } = req.body;
-  const user = req.user;
-
-  if (!activityName || !phone || !activityTime || !bookingDate) {
-    return res.status(400).send({ Message: "All fields must be provided" });
-  }
-
   try {
+    const { id } = req.params;
+    const { activityName, phone, activityTime, bookingDate } = req.body;
+
+    if (!activityName || !phone || !activityTime || !bookingDate) {
+      return res.status(400).send({ message: "All fields must be provided" });
+    }
+
     const activity = await Activity.findById(id);
     if (!activity) {
-      return res.status(404).send({ Message: "Activity Not Found" });
+      return res.status(404).send({ message: "Activity Not Found" });
     }
 
     activity.activityName = activityName;
@@ -68,11 +85,13 @@ export const Updateactivity = async (req, res) => {
     activity.bookingDate = bookingDate;
     await activity.save();
 
-    return res.status(200).send({ Message: "Activity Updated Successfully" });
+    return res
+      .status(200)
+      .send({ message: "Activity Updated Successfully", success: true });
   } catch (error) {
     return res
       .status(500)
-      .send({ Message: "An error occurred", error: error.message });
+      .send({ message: "An error occurred", error: error.message });
   }
 };
 
@@ -82,9 +101,13 @@ export const deleteactivity = async (req, res) => {
   try {
     const deleted = await Activity.findByIdAndDelete(id);
     if (!deleted) {
-      return res.status(404).send({ Message: "Activity Not Found" });
+      return res
+        .status(404)
+        .send({ message: "Activity Not Found", success: false });
     }
-    return res.status(200).send({ Message: "Activity Deleted Successfully" });
+    return res
+      .status(200)
+      .send({ message: "Activity Deleted Successfully", success: true });
   } catch (error) {
     console.log(error.message);
   }
